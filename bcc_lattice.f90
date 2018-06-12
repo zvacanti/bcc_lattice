f7b197142d5ca4a0852d71d41d39bbeb4b0432aa
zvacanti <39600921+zvacanti@users.noreply.github.com>
2018-06-12 13:31:36 -0400

    Create bcc_lattice.f90

---
+++ bcc_lattice.f90
@@ -0,0 +1,16 @@
+PROGRAM bcc_lattice
+  IMPLICIT NONE
+  REAL :: a,b,c, Area
+  PRINT *, 'Please enter the side lengths.'
+  READ *, a, b, c
+  PRINT *, 'Triangle''s Area: ', Area(abc)
+END PROGRAM bcc_lattice
+
+Function Area(x,y,z)
+  IMPLICIT NONE
+  REAL :: Area
+  REAL, INTENT ( IN ) :: x, y, z
+  REAL :: theta, height
+  theta = ACOS((x**2 + y**2 - z**2)/(2.0*x*y))
+  height = x*SIN(theta); Area = 0.5*y*height
+END FUNCTION Area
