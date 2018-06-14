10d11f1a6c3eb92e9b9a0f4eff8bae0a6f846697
schuht1 <40212463+schuht1@users.noreply.github.com>
2018-06-14 17:03:03 -0400

    Add files via upload

--- simple_crystal_lattice.f90
+++ simple_crystal_lattice.f90
@@ -2,15 +2,14 @@ program simple_crystal_lattice
 
 implicit none
 
-integer :: a
+integer :: a !a = n, the number of ions on a side
 integer :: i
 integer :: j
 integer :: k
-integer :: p
-integer :: q
-integer :: r
 integer :: N  !number of particles
+integer :: counter
 real :: s     !side lengths of crystal
+real :: rho
 real :: x
 real :: y
 real :: z
@@ -18,54 +17,52 @@ real :: x_cen
 real :: y_cen
 real :: z_cen
 
-N = 35	!N = particles on the vertices of each cube + particles at the center of each cube
+!N = 54	!N = particles on the vertices of each cube + particles at the center of each cube
+
+print  *, 'Enter an integer number of ions, N'
+read  *, N
+a = ceiling((N/2.0)**(1.0/3.0))
+counter = 0
+print  *, 'The number of ions is', N
+print  *, 'The value of a is', a
+
 s = 1
+!print  *, 'Enter a real density value, rho'
+!read  *, rho
+!s = ((2)/(rho))**(1.0/3.0)
+!print  *, 'The value of s is', s
 
-open(unit = 2, file = "simple_crystal.xyz")
+open(unit = 2, file = "simple_crystal.txt")
 
-write(2,*) N
-write(2,*)
-
-!!!MAKE SURE THAT p,q,r are all one less than i,j,k
+!write(2,*) N
+!write(2,*)
 
 !!!particles on the vertices of each cube 
 !!!# of particles = # of loops performed by i * # of loops performedd by j * # of loops performed by k
 !!!e.g. i=0,2 j=0,2 k=0,2 which is  3*3*3 = 27 particles
 
-	do i=0,2	!change the end term of each do while loop to get larger figures
-		x = i*s	!for perfect cubes i=j=k, e.g. for a 2x2x2 cube let i=j=k=2
-
-		do j=0,2
-			y = j*s
-
-			do k=0,2
-				z = k*s
-				write(2,*) "C", x, y, z
+	do i=0,a-1	        	!change the end term of each do while loop to get larger figures
+		x = i*s + s/4		!for perfect cubes i=j=k, e.g. for a 2x2x2 cube let i=j=k=2
+		x_cen = i*s + s/2 + s/4
+		do j=0,a-1
+			y = j*s + s/4
+			y_cen = j*s + s/2 + s/4
+			do k=0,a-1
+				if (counter .EQ. N) EXIT
+				z = k*s + s/4
+				z_cen = k*s + s/2 + s/4
+				write(2,*) x, y, z
+				counter = counter + 1
+				if (counter .EQ. N) EXIT
+				write(2,*) x_cen, y_cen, z_cen
+				counter = counter + 1
+				if (counter .EQ. N) EXIT
 			end do
 		
 		end do	
-
+	
 	end do
 
-write(2,*)
-
-!!!particles at the center of each cube
-!!!# of particles = # of loops performed by p * # of loops performedd by q * # of loops performed by r
-!!!e.g. p=0,1 q=0,1 r=0,1 which is  2*2*2 = 8 particles
-
-	do p=0,1
-		x_cen = p*s + s/2
-
-		do q=0,1
-			y_cen = q*s + s/2
-
-			do r=0,1
-				z_cen = r*s + s/2
-				write(2,*) "C", x_cen, y_cen, z_cen
-			end do
-
-		end do
-
-	end do
+print  *, counter
 
 end  program simple_crystal_lattice
